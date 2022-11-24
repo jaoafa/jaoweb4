c9f5975c94948f191d4e0ed734edf3aab068f180
Hiratake <hiratake@hiratake.xyz>
2022-11-24 20:12:01 +0900

    build: sassを使用できるよう設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // nuxt configuration
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  content: {},
+  modules: ['@nuxt/content'],
   srcDir: 'src/',
   typescript: { shim: false, strict: true },
-  modules: ['@nuxt/content'],
-  content: {},
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: { additionalData: '@use "@/assets/styles" as *;' },
+      },
+    },
+  },
 })
