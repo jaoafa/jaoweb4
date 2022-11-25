ae95156f6569e77a7f51705b8cb0213cfb386cfe
Hiratake <hiratake@hiratake.xyz>
2022-11-25 23:46:52 +0900

    chore: ナビゲーションの項目をapp.configへ移動

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,87 +1,7 @@
 <script setup lang="ts">
 import { siDiscord } from 'simple-icons/icons'
 
-const navItems = computed(
-  (): {
-    label: string
-    to?: string
-    href?: string
-    children?: {
-      label: string
-      description: string
-      to?: string
-      href?: string
-    }[]
-  }[] => [
-    { label: 'はじめての方へ', to: '/guide' },
-    {
-      label: 'ドキュメント',
-      children: [
-        {
-          label: 'サービス仕様',
-          description: 'Minecraftサーバの仕様をみる',
-          to: '/docs/spec',
-        },
-        {
-          label: 'ガイドライン',
-          description: '一緒に楽しむためのガイドラインをみる',
-          to: '/docs/guidelines',
-        },
-        {
-          label: '利用規約',
-          description: 'サービス利用時のルールをみる',
-          to: '/docs/terms',
-        },
-        {
-          label: '運営方針',
-          description: '運営チームが目指す方針をみる',
-          to: '/docs/policies',
-        },
-      ],
-    },
-    {
-      label: 'コミュニティ',
-      children: [
-        {
-          label: 'ブログ',
-          description: 'コミュニティの活動をみる',
-          to: '/community/blog',
-        },
-        {
-          label: 'お知らせ',
-          description: '運営チームからのお知らせをみる',
-          to: '/community/news',
-        },
-        {
-          label: 'wiki',
-          description: '最高にイカした百科事典をみる',
-          href: 'https://wiki.jaoafa.com/',
-        },
-      ],
-    },
-    {
-      label: 'サポート',
-      children: [
-        {
-          label: 'お問い合わせ',
-          description: '困ったときのお問合せ先をみる',
-          to: '/support/inquiry',
-        },
-        {
-          label: 'よくあるご質問',
-          description: 'よくあるお困りごとの一覧をみる',
-          to: '/support/faq',
-        },
-        {
-          label: '運営者情報',
-          description: '運営チームの情報をみる',
-          to: '/support/profiles',
-        },
-      ],
-    },
-  ]
-)
-
+const config = useAppConfig()
 const showOverlayMenu = ref<boolean>(false)
 </script>
 
@@ -98,19 +18,12 @@ const showOverlayMenu = ref<boolean>(false)
           class="header__nav"
         >
           <ul>
-            <template v-for="(item, index) in navItems" :key="index">
+            <template v-for="(item, index) in config.nav" :key="index">
               <li>
-                <template v-if="item.to || item.href">
-                  <template v-if="item.to">
-                    <NuxtLink :to="item.to">
-                      {{ item.label }}
-                    </NuxtLink>
-                  </template>
-                  <template v-else>
-                    <a :href="item.href" target="_blank">
-                      {{ item.label }}
-                    </a>
-                  </template>
+                <template v-if="item.to">
+                  <NuxtLink :to="item.to">
+                    {{ item.label }}
+                  </NuxtLink>
                 </template>
 
                 <template v-else>
