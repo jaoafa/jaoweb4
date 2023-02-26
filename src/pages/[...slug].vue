84bf9950e2188493fd683fe7a6a01434efe52158
Hiratake <hiratake@hiratake.xyz>
2023-02-27 07:51:06 +0900

    chore: 各ページのmdファイルを追加

--- src/pages/[...slug].vue
+++ src/pages/[...slug].vue
@@ -1,6 +1,9 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      <h1>{{ doc.title }}</h1>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
   </main>
 </template>
 
