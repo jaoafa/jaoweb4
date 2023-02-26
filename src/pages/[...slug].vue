5d3223d4a7685edee0a9d0147c6340ed3aba36ba
Hiratake <hiratake@hiratake.xyz>
2023-02-26 21:23:43 +0900

    chore: 記事ページの外枠を追加

--- src/pages/[...slug].vue
+++ src/pages/[...slug].vue
@@ -3,3 +3,16 @@
     <ContentDoc />
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  box-sizing: content-box;
+  max-width: var(--max);
+  padding: 24px 16px;
+  margin: auto;
+
+  @include breakpoint {
+    padding: 32px 24px;
+  }
+}
+</style>
