2b135f1185ac19dcd1dbee13cc9d964698417413
Hiratake <hiratake@hiratake.xyz>
2023-03-03 14:58:07 +0900

    feat: 記事ページを追加

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,7 +8,7 @@ const { y } = useWindowScroll()
   <header
     :class="`
       ${'fixed top-0 z-40 w-full transition'}
-      ${y > 0 ? 'bg-white/30 shadow-md backdrop-blur' : 'bg-transparent'}
+      ${y > 0 ? 'bg-white/50 shadow-md backdrop-blur' : 'bg-transparent'}
     `"
   >
     <div class="m-auto box-content max-w-6xl py-3 px-4 md:py-4 md:px-6">
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mx-auto max-w-3xl">
-    <main>
-      <article>
-        <slot />
-      </article>
-    </main>
-  </div>
+  <main
+    class="m-auto box-content max-w-3xl px-4 pt-24 pb-10 md:px-6 md:pb-16 md:pt-36"
+  >
+    <article>
+      <slot />
+    </article>
+  </main>
 </template>
