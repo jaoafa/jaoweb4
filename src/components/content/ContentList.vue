1d78fe1b1938e390fdb5e3d24135ef2034dcc777
Hiratake <hiratake@hiratake.xyz>
2023-03-04 17:18:32 +0900

    chore: リスト表示の見た目を調整

--- src/components/content/ContentList.vue
+++ src/components/content/ContentList.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { CalendarDaysIcon } from '@heroicons/vue/20/solid'
+
 const props = defineProps<{
   query: string[]
 }>()
-
 const { data } = await useAsyncData(
   `content-list-${props.query.join('-')}`,
   () => {
@@ -18,33 +19,53 @@ const { data } = await useAsyncData(
 
 <template>
   <div class="not-prose">
-    <ul>
+    <ul class="grid-cols-fill-56 grid gap-6">
       <template v-for="item in data" :key="item._path">
-        <li>
+        <li
+          class="hover:border-primary-600 rounded-lg border border-gray-100 bg-white p-3 transition-colors"
+        >
           <NuxtLink :to="item._path">
-            <article>
-              <h2>{{ item.title }}</h2>
-              <p>
-                {{
-                  useDateFormat(item.created, 'MMMM DD, YYYY', {
-                    locales: 'en-US',
-                  }).value
-                }}
-              </p>
-              <ul class="flex flex-wrap gap-2">
-                <template v-for="(author, index) in item.author" :key="index">
-                  <li
-                    class="aspect-square w-7 overflow-hidden rounded-full bg-gray-100"
-                  >
-                    <img
-                      v-if="author.icon"
-                      :src="author.icon"
-                      alt="icon"
-                      class="h-full w-full object-cover"
-                    />
-                  </li>
-                </template>
-              </ul>
+            <article class="flex h-full flex-col justify-between gap-3">
+              <h2 class="font-bold">{{ item.title }}</h2>
+              <div class="flex flex-wrap items-center justify-between gap-2">
+                <dl
+                  v-if="item.created"
+                  class="flex items-center gap-1 text-xs text-gray-500"
+                >
+                  <dt class="translate-y-[0.1em]" aria-label="Posted on">
+                    <AppIcon size="12px">
+                      <CalendarDaysIcon />
+                    </AppIcon>
+                  </dt>
+                  <dd>
+                    <time
+                      :datetime="
+                        useDateFormat(item.created, 'YYYY-MM-DD').value
+                      "
+                    >
+                      {{
+                        useDateFormat(item.created, 'MMMM DD, YYYY', {
+                          locales: 'en-US',
+                        }).value
+                      }}
+                    </time>
+                  </dd>
+                </dl>
+                <ul class="flex flex-wrap gap-2">
+                  <template v-for="(author, index) in item.author" :key="index">
+                    <li
+                      class="aspect-square w-7 overflow-hidden rounded-full bg-gray-100"
+                    >
+                      <img
+                        v-if="author.icon"
+                        :src="author.icon"
+                        alt="icon"
+                        class="h-full w-full object-cover"
+                      />
+                    </li>
+                  </template>
+                </ul>
+              </div>
             </article>
           </NuxtLink>
         </li>
---
+++ src/layouts/list.vue
@@ -0,0 +1,7 @@
+<template>
+  <main
+    class="m-auto box-content max-w-6xl px-4 pt-24 pb-10 md:px-6 md:pb-16 md:pt-36"
+  >
+    <slot />
+  </main>
+</template>
