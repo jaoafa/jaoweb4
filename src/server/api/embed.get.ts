ab7fef99e52b19e90a1ae96b6517601cc9b17da4
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-03-13 08:42:15 +0900

    feat: ogp-parserに依存せず独自でメタタグを取得するように

--- src/server/api/embed.get.ts
+++ src/server/api/embed.get.ts
@@ -1,4 +1,55 @@
-import parser from 'ogp-parser'
+import { CheerioAPI, load } from 'cheerio'
+
+interface MetaItems {
+  [key: string]: string[]
+}
+
+interface PageMeta {
+  pageTitle: string
+  ogp: MetaItems
+  seo: MetaItems
+}
+
+async function getHTML(url: string): Promise<string> {
+  try {
+    const res = await fetch(url, {
+      redirect: 'follow',
+    })
+    return await res.text()
+  } catch (error) {
+    throw new Error(`Failed to fetch ${url}: ${JSON.stringify(error)}`)
+  }
+}
+
+function getPageMeta($: CheerioAPI): PageMeta {
+  const title = $('title').text()
+
+  const meta = $('head meta')
+  const ogp: MetaItems = {}
+  const seo: MetaItems = {}
+  for (let i = 0; i < meta.length; i++) {
+    const element = meta[i]
+    const isOgp = 'property' in element.attribs
+    const isSeo = 'name' in element.attribs
+
+    if (isOgp) {
+      const key = element.attribs.property
+      const value = element.attribs.content
+      ogp[key] = ogp[key] || []
+      ogp[key].push(value)
+    } else if (isSeo) {
+      const key = element.attribs.name
+      const value = element.attribs.content
+      seo[key] = seo[key] || []
+      seo[key].push(value)
+    }
+  }
+  return {
+    pageTitle: title,
+    ogp,
+    seo,
+  }
+}
 
 function getFirst<T>(array: T[] | undefined): T | undefined {
   if (!array) {
@@ -17,28 +68,36 @@ export default defineEventHandler(async (event) => {
     return { error: 'url is invalid' }
   }
 
-  const result = await parser(url)
-  const siteName = getFirst(result.ogp['og:site_name'])
-  const domain = new URL(url).hostname
-  const siteIcon = `https://www.google.com/s2/favicons?domain=${domain}`
-  const title = getFirst(result.ogp['og:title']) || result.title
-  const description =
-    getFirst(result.seo.description) || getFirst(result.ogp['og:description'])
-  const image = getFirst(result.ogp['og:image'])
-  const twitterCard =
-    getFirst(result.seo['twitter:card']) || getFirst(result.ogp['twitter:card'])
+  try {
+    const html = await getHTML(url)
+    const $ = load(html)
+    const result = getPageMeta($)
 
-  return {
-    site: {
-      name: siteName,
-      icon: siteIcon,
-      domain,
-    },
-    page: {
-      title,
-      description,
-      image,
-      twitterCard,
-    },
+    const siteName = getFirst(result.ogp['og:site_name'])
+    const domain = new URL(url).hostname
+    const siteIcon = `https://www.google.com/s2/favicons?domain=${domain}`
+    const title = getFirst(result.ogp['og:title']) || result.pageTitle
+    const description =
+      getFirst(result.seo.description) || getFirst(result.ogp['og:description'])
+    const image = getFirst(result.ogp['og:image'])
+    const twitterCard =
+      getFirst(result.seo['twitter:card']) ||
+      getFirst(result.ogp['twitter:card'])
+
+    return {
+      site: {
+        name: siteName,
+        icon: siteIcon,
+        domain,
+      },
+      page: {
+        title,
+        description,
+        image,
+        twitterCard,
+      },
+    }
+  } catch (error) {
+    return { error: (error as Error).message || 'Unknown error' }
   }
 })
