84b59d6b98e0f80e0ad3bfde8c82877cb391e5cf
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-03-13 08:08:38 +0900

    feat: カードをリンク化。記事情報を取得できなかった場合はURLだけを表示する

--- src/components/content/ContentEmbed.vue
+++ src/components/content/ContentEmbed.vue
@@ -11,40 +11,45 @@ const { data } = useFetch('/api/embed', {
 </script>
 
 <template>
-  <div v-if="data && data.page && data.site">
-    <div class="flex w-full overflow-hidden rounded-lg shadow-lg">
-      <div class="w-2/3 p-2 lg:p-4">
-        <p class="text-1xl font-bold text-gray-900">
-          {{ data.page.title }}
-        </p>
-        <p class="text-sm text-gray-700">
-          {{ data.page.description }}
-        </p>
-        <div class="site-info flex items-center gap-2">
-          <div>
-            <img
-              :src="data.site.icon"
-              :alt="`${data.site.domain} favicon`"
-              class="h-full w-full overflow-hidden bg-gray-100"
-            />
-          </div>
-          <p class="text-sm text-gray-700">
-            {{ data.site.name }}
+  <a
+    :href="props.url"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="no-underline"
+  >
+    <div v-if="data && data.page && data.site">
+      <div class="flex w-full overflow-hidden rounded-lg shadow-lg">
+        <div class="w-2/3 p-2 lg:p-4">
+          <p class="text-1xl font-bold text-gray-900">
+            {{ data.page.title }}
           </p>
+          <p class="text-sm text-gray-700">
+            {{ data.page.description }}
+          </p>
+          <div class="site-info flex items-center gap-2">
+            <div>
+              <img
+                :src="data.site.icon"
+                :alt="`${data.site.domain} favicon`"
+                class="h-full w-full overflow-hidden bg-gray-100"
+              />
+            </div>
+            <p class="text-sm text-gray-700">
+              {{ data.site.name }}
+            </p>
+          </div>
+        </div>
+        <div v-if="data.page.image" class="w-1/3">
+          <img
+            :src="data.page.image"
+            :alt="`${data.page.title} thumbnail`"
+            class="h-full w-full object-cover"
+          />
         </div>
       </div>
-      <div v-if="data.page.image" class="w-1/3">
-        <img
-          :src="data.page.image"
-          :alt="`${data.page.title} thumbnail`"
-          class="h-full w-full object-cover"
-        />
-      </div>
     </div>
-  </div>
-  <div v-else>
-    <p class="text-red-500">
-      {{ data ? data.error : 'Failed to fetch data.' }}
-    </p>
-  </div>
+    <div v-else>
+      {{ props.url }}
+    </div>
+  </a>
 </template>
