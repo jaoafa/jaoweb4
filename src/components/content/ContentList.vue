7aff81c851e8f7c50a67b7f2550fcd1d57f22886
Hiratake <hiratake@hiratake.xyz>
2023-03-21 21:58:15 +0900

    feat: 著者名で絞り込みする機能を追加

--- src/components/content/ContentList.vue
+++ src/components/content/ContentList.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
-import { CalendarDaysIcon } from '@heroicons/vue/20/solid'
+import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import {
+  CalendarDaysIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
+} from '@heroicons/vue/20/solid'
 
 const props = withDefaults(
   defineProps<{
@@ -10,25 +21,118 @@ const props = withDefaults(
     filter: false,
   }
 )
+
+interface Article extends MarkdownParsedContent {
+  created?: string
+  updated?: string
+  author?: {
+    name?: string
+    icon?: string
+  }[]
+}
 const { data } = await useAsyncData(
   `content-list-${props.query.join('-')}`,
   () => {
     const [query, ...pathParts] = props.query
-    return queryContent(query, ...pathParts)
+    return queryContent<Article>(query, ...pathParts)
       .where({ _dir: { $eq: pathParts[pathParts.length - 1] || query } })
-      .only(['_path', 'title', 'created', 'author'])
       .sort({ created: -1 })
       .find()
   }
 )
+const authors = computed<{ id: number; name: string }[]>(() => [
+  { id: 0, name: '全て' },
+  ...new Set(
+    data.value
+      ?.map((article) => article.author?.map((item) => item.name || '') || [])
+      .flat()
+      .map((item, index) => ({ id: index + 1, name: item })) || []
+  ),
+])
+const selectedAuthor = ref(authors.value[0])
+const filteredData = computed(
+  () =>
+    data.value?.filter((article) => {
+      // 著者で絞り込み
+      const isAuthor: boolean =
+        article.author?.some(
+          (author) => author.name === selectedAuthor.value.name
+        ) || selectedAuthor.value.name === '全て'
+      return isAuthor
+    }) || []
+)
 </script>
 
 <template>
-  <div class="not-prose grid gap-3">
-    <div v-if="props.filter"></div>
+  <div class="not-prose grid gap-6">
+    <div v-if="props.filter">
+      <div class="relative z-10 w-40">
+        <Listbox v-model="selectedAuthor">
+          <div class="relative">
+            <ListboxButton
+              class="relative w-full rounded-2xl border border-gray-200 bg-gray-50 py-1 pl-3 pr-10 text-xs hover:bg-gray-100 focus:outline-none focus-visible:bg-gray-100"
+            >
+              <span class="flex items-center gap-2 truncate">
+                <span>著者：</span>
+                <span>{{ selectedAuthor.name }}</span>
+              </span>
+              <span
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+              >
+                <ChevronUpDownIcon
+                  class="h-4 w-4 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </ListboxButton>
 
-    <ul v-if="data?.length" class="grid grid-cols-fill-56 gap-6">
-      <template v-for="item in data" :key="item._path">
+            <Transition
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-gray-900 ring-opacity-5 focus:outline-none"
+              >
+                <ListboxOption
+                  v-for="author in authors"
+                  v-slot="{ active, selected }"
+                  :key="author.id"
+                  :value="author"
+                  as="template"
+                >
+                  <li
+                    :class="`
+                      ${'relative cursor-pointer select-none py-2 pl-10 pr-4 text-gray-500 transition-colors'}
+                      ${active ? 'bg-gray-50 text-gray-800' : ''}
+                      ${selected ? 'text-gray-800' : ''}
+                    `"
+                  >
+                    <span
+                      :class="`
+                        ${'block truncate'}
+                        ${selected ? 'font-bold' : 'font-normal'}
+                      `"
+                    >
+                      {{ author.name }}
+                    </span>
+                    <span
+                      v-if="selected"
+                      class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
+                    >
+                      <CheckIcon class="h-4 w-4" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
+    </div>
+
+    <ul v-if="filteredData?.length" class="grid grid-cols-fill-56 gap-6">
+      <template v-for="item in filteredData" :key="item._path">
         <li
           class="rounded-lg border border-gray-100 bg-white p-3 transition-colors hover:border-primary-600"
         >
