4cad7fe7ffc0db614aafafee05a98f9ecbc3e57c
Hiratake <hiratake@hiratake.xyz>
2023-03-22 14:57:16 +0900

    refactor: 記事一覧コンポーネントのリファクタリング

--- src/components/content/ContentList.vue
+++ src/components/content/ContentList.vue
@@ -1,17 +1,5 @@
 <script setup lang="ts">
 import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
-import {
-  Listbox,
-  ListboxButton,
-  ListboxOptions,
-  ListboxOption,
-} from '@headlessui/vue'
-import {
-  CalendarDaysIcon,
-  CheckIcon,
-  ChevronUpDownIcon,
-  TagIcon,
-} from '@heroicons/vue/20/solid'
 
 const props = withDefaults(
   defineProps<{
@@ -28,7 +16,7 @@ interface Article extends MarkdownParsedContent {
   updated?: string
   tag?: string[]
   author?: {
-    name?: string
+    name: string
     icon?: string
   }[]
 }
@@ -42,37 +30,21 @@ const { data } = await useAsyncData(
       .find()
   }
 )
-const authors = computed<{ id: number; name: string }[]>(() =>
-  [
-    '全て',
-    ...new Set(
-      data.value
-        ?.map((article) => article.author?.map((item) => item.name || '') || [])
-        .flat() || []
-    ),
-  ].map((item, index) => ({ id: index, name: item }))
-)
-const selectedAuthor = ref(authors.value[0])
-const tags = computed<{ id: number; name: string }[]>(
-  () =>
-    [
-      '全て',
-      ...new Set(data.value?.map((article) => article.tag || []).flat() || []),
-    ].map((item, index) => ({ id: index, name: item })) || []
-)
-const selectedTag = ref(tags.value[0])
+
+const selectedAuthor = ref('全て')
+const selectedTag = ref('全て')
 const filteredData = computed(
   () =>
     data.value?.filter((article) => {
       // 著者で絞り込み
       const isAuthor: boolean =
         article.author?.some(
-          (author) => author.name === selectedAuthor.value.name
-        ) || selectedAuthor.value.name === '全て'
+          (author) => author.name === selectedAuthor.value
+        ) || selectedAuthor.value === '全て'
       // タグで絞り込み
       const isTag: boolean =
-        article.tag?.some((tag) => tag === selectedTag.value.name) ||
-        selectedTag.value.name === '全て'
+        article.tag?.some((tag) => tag === selectedTag.value) ||
+        selectedTag.value === '全て'
       return isAuthor && isTag
     }) || []
 )
@@ -80,197 +52,25 @@ const filteredData = computed(
 
 <template>
   <div class="not-prose grid gap-6">
-    <div v-if="props.filter" class="flex flex-wrap items-center gap-2">
-      <div class="relative z-10 w-40">
-        <Listbox v-model="selectedAuthor">
-          <div class="relative">
-            <ListboxButton
-              class="relative w-full rounded-2xl border border-gray-200 bg-gray-50 py-1 pl-3 pr-10 text-xs hover:bg-gray-100 focus:outline-none focus-visible:bg-gray-100"
-            >
-              <span class="flex items-center gap-2 truncate">
-                <span>著者：</span>
-                <span>{{ selectedAuthor.name }}</span>
-              </span>
-              <span
-                class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
-              >
-                <ChevronUpDownIcon
-                  class="h-4 w-4 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </ListboxButton>
-
-            <Transition
-              leave-active-class="transition duration-100 ease-in"
-              leave-from-class="opacity-100"
-              leave-to-class="opacity-0"
-            >
-              <ListboxOptions
-                class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-gray-900 ring-opacity-5 focus:outline-none"
-              >
-                <ListboxOption
-                  v-for="author in authors"
-                  v-slot="{ active, selected }"
-                  :key="author.id"
-                  :value="author"
-                  as="template"
-                >
-                  <li
-                    :class="`
-                      ${'relative cursor-pointer select-none py-2 pl-10 pr-4 text-gray-500 transition-colors'}
-                      ${active ? 'bg-gray-50 text-gray-800' : ''}
-                      ${selected ? 'text-gray-800' : ''}
-                    `"
-                  >
-                    <span
-                      :class="`
-                        ${'block truncate'}
-                        ${selected ? 'font-bold' : 'font-normal'}
-                      `"
-                    >
-                      {{ author.name }}
-                    </span>
-                    <span
-                      v-if="selected"
-                      class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
-                    >
-                      <CheckIcon class="h-4 w-4" aria-hidden="true" />
-                    </span>
-                  </li>
-                </ListboxOption>
-              </ListboxOptions>
-            </Transition>
-          </div>
-        </Listbox>
-      </div>
-
-      <div class="relative z-10 w-40">
-        <Listbox v-model="selectedTag">
-          <div class="relative">
-            <ListboxButton
-              class="relative w-full rounded-2xl border border-gray-200 bg-gray-50 py-1 pl-3 pr-10 text-xs hover:bg-gray-100 focus:outline-none focus-visible:bg-gray-100"
-            >
-              <span class="flex items-center gap-2 truncate">
-                <span>タグ：</span>
-                <span>{{ selectedTag.name }}</span>
-              </span>
-              <span
-                class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
-              >
-                <ChevronUpDownIcon
-                  class="h-4 w-4 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </ListboxButton>
-
-            <Transition
-              leave-active-class="transition duration-100 ease-in"
-              leave-from-class="opacity-100"
-              leave-to-class="opacity-0"
-            >
-              <ListboxOptions
-                class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-gray-900 ring-opacity-5 focus:outline-none"
-              >
-                <ListboxOption
-                  v-for="tag in tags"
-                  v-slot="{ active, selected }"
-                  :key="tag.id"
-                  :value="tag"
-                  as="template"
-                >
-                  <li
-                    :class="`
-                      ${'relative cursor-pointer select-none py-2 pl-10 pr-4 text-gray-500 transition-colors'}
-                      ${active ? 'bg-gray-50 text-gray-800' : ''}
-                      ${selected ? 'text-gray-800' : ''}
-                    `"
-                  >
-                    <span
-                      :class="`
-                        ${'block truncate'}
-                        ${selected ? 'font-bold' : 'font-normal'}
-                      `"
-                    >
-                      {{ tag.name }}
-                    </span>
-                    <span
-                      v-if="selected"
-                      class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
-                    >
-                      <CheckIcon class="h-4 w-4" aria-hidden="true" />
-                    </span>
-                  </li>
-                </ListboxOption>
-              </ListboxOptions>
-            </Transition>
-          </div>
-        </Listbox>
-      </div>
-    </div>
+    <template v-if="props.filter">
+      <ContentListHeader
+        v-model:author="selectedAuthor"
+        v-model:tag="selectedTag"
+        :content="data || undefined"
+      />
+    </template>
 
     <ul v-if="filteredData?.length" class="grid grid-cols-fill-56 gap-6">
       <template v-for="item in filteredData" :key="item._path">
-        <li
-          class="rounded-lg border border-gray-100 bg-white p-3 transition-colors hover:border-primary-600"
-        >
-          <NuxtLink :to="item._path">
-            <article class="flex h-full flex-col justify-between gap-3">
-              <h2 class="h-[3.5em] font-bold line-clamp-2">
-                {{ item.title }}
-              </h2>
-              <div class="flex flex-wrap items-center justify-between gap-2">
-                <dl
-                  v-if="item.created || item.tag"
-                  class="grid gap-1 text-xs text-gray-500"
-                >
-                  <div v-if="item.created" class="flex items-center gap-1">
-                    <dt class="translate-y-[0.1em]" aria-label="Posted on">
-                      <AppIcon size="12px">
-                        <CalendarDaysIcon />
-                      </AppIcon>
-                    </dt>
-                    <dd>
-                      <time
-                        :datetime="
-                          useDateFormat(item.created, 'YYYY-MM-DD').value
-                        "
-                      >
-                        {{
-                          useDateFormat(item.created, 'MMMM DD, YYYY', {
-                            locales: 'en-US',
-                          }).value
-                        }}
-                      </time>
-                    </dd>
-                  </div>
-                  <div v-if="item.tag" class="flex items-center gap-1">
-                    <dt class="translate-y-[0.1em]" aria-label="Tag">
-                      <AppIcon size="12px">
-                        <TagIcon />
-                      </AppIcon>
-                    </dt>
-                    <dd>{{ item.tag.join(', ') }}</dd>
-                  </div>
-                </dl>
-                <ul class="flex flex-wrap gap-2">
-                  <template v-for="(author, index) in item.author" :key="index">
-                    <li
-                      class="aspect-square w-7 overflow-hidden rounded-full bg-gray-100"
-                    >
-                      <img
-                        v-if="author.icon"
-                        :src="author.icon"
-                        alt="icon"
-                        class="h-full w-full object-cover"
-                      />
-                    </li>
-                  </template>
-                </ul>
-              </div>
-            </article>
-          </NuxtLink>
+        <li v-if="item._path">
+          <ContentListItem
+            :url="item._path"
+            :title="item.title"
+            :created="item.created"
+            :updated="item.updated"
+            :tag="item.tag"
+            :author="item.author"
+          />
         </li>
       </template>
     </ul>
---
+++ src/components/content/ContentListHeader.vue
@@ -0,0 +1,187 @@
+<script setup lang="ts">
+import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+
+interface Article extends MarkdownParsedContent {
+  tag?: string[]
+  author?: {
+    name: string
+    icon?: string
+  }[]
+}
+const props = withDefaults(
+  defineProps<{ content?: Article[]; author?: string; tag?: string }>(),
+  {
+    content: () => [],
+    author: '全て',
+    tag: '全て',
+  }
+)
+const emit = defineEmits<{
+  (e: 'update:author', val: Required<Article>['author'][number]['name']): void
+  (e: 'update:tag', val: Required<Article>['tag'][number]): void
+}>()
+
+// 著者
+const authors = computed<{ id: number; name: string }[]>(() =>
+  [
+    '全て',
+    ...new Set(
+      props.content
+        .map((article) => article.author?.map((item) => item.name) || [])
+        .flat() || []
+    ),
+  ].map((item, index) => ({ id: index, name: item }))
+)
+const modelAuthor = computed({
+  get: () =>
+    authors.value.find((item) => item.name === props.author) ||
+    authors.value[0],
+  set: (val) => emit('update:author', val.name),
+})
+
+// タグ
+const tags = computed<{ id: number; name: string }[]>(() =>
+  [
+    '全て',
+    ...new Set(props.content.map((article) => article.tag || []).flat() || []),
+  ].map((item, index) => ({ id: index, name: item }))
+)
+const modelTag = computed({
+  get: () =>
+    tags.value.find((item) => item.name === props.tag) || tags.value[0],
+  set: (val) => emit('update:tag', val.name),
+})
+</script>
+
+<template>
+  <div class="flex flex-wrap items-center gap-3">
+    <Listbox v-model="modelAuthor">
+      <div class="relative w-40">
+        <ListboxButton
+          class="relative w-full rounded-2xl border border-gray-200 bg-gray-50 py-1 pl-3 pr-10 text-xs hover:bg-gray-100 focus:outline-none focus-visible:bg-gray-100"
+        >
+          <span class="flex items-center gap-2 truncate">
+            <span>著者：</span>
+            <span>{{ modelAuthor.name }}</span>
+          </span>
+          <span
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <ChevronUpDownIcon
+              class="h-4 w-4 text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
+        </ListboxButton>
+        <Transition
+          leave-active-class="transition duration-100 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <ListboxOptions
+            class="absolute z-40 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-gray-900 ring-opacity-5 focus:outline-none"
+          >
+            <ListboxOption
+              v-for="item in authors"
+              v-slot="{ active, selected }"
+              :key="item.id"
+              :value="item"
+              as="template"
+            >
+              <li
+                :class="`
+                ${'relative cursor-pointer select-none py-2 pl-10 pr-4 text-gray-500 transition-colors'}
+                ${active ? 'bg-gray-50 text-gray-800' : ''}
+                ${selected ? 'text-gray-800' : ''}
+              `"
+              >
+                <span
+                  :class="`
+                    ${'block truncate'}
+                    ${selected ? 'font-bold' : 'font-normal'}
+                  `"
+                >
+                  {{ item.name }}
+                </span>
+                <span
+                  v-show="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
+                >
+                  <CheckIcon class="h-4 w-4" aria-hidden="true" />
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </Transition>
+      </div>
+    </Listbox>
+
+    <Listbox v-model="modelTag">
+      <div class="relative z-10 w-40">
+        <ListboxButton
+          class="relative w-full rounded-2xl border border-gray-200 bg-gray-50 py-1 pl-3 pr-10 text-xs hover:bg-gray-100 focus:outline-none focus-visible:bg-gray-100"
+        >
+          <span class="flex items-center gap-2 truncate">
+            <span>タグ：</span>
+            <span>{{ modelTag.name }}</span>
+          </span>
+          <span
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <ChevronUpDownIcon
+              class="h-4 w-4 text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
+        </ListboxButton>
+        <Transition
+          leave-active-class="transition duration-100 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <ListboxOptions
+            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-gray-900 ring-opacity-5 focus:outline-none"
+          >
+            <ListboxOption
+              v-for="item in tags"
+              v-slot="{ active, selected }"
+              :key="item.id"
+              :value="item"
+              as="template"
+            >
+              <li
+                :class="`
+                ${'relative cursor-pointer select-none py-2 pl-10 pr-4 text-gray-500 transition-colors'}
+                ${active ? 'bg-gray-50 text-gray-800' : ''}
+                ${selected ? 'text-gray-800' : ''}
+              `"
+              >
+                <span
+                  :class="`
+                    ${'block truncate'}
+                    ${selected ? 'font-bold' : 'font-normal'}
+                  `"
+                >
+                  {{ item.name }}
+                </span>
+                <span
+                  v-show="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600"
+                >
+                  <CheckIcon class="h-4 w-4" aria-hidden="true" />
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </Transition>
+      </div>
+    </Listbox>
+  </div>
+</template>
