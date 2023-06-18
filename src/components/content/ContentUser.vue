3e0bc63e9cedfe73274a2334f80415194b203c59
Hiratake <hiratake@hiratake.xyz>
2023-06-19 00:55:22 +0900

    feat: ユーザ情報を表示するコンポーネントを追加

---
+++ src/components/content/ContentUser.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { siDiscord, siTwitter } from 'simple-icons'
+
+const props = withDefaults(
+  defineProps<{
+    /** ユーザの名前を指定します */
+    name: string
+    /** アイコン画像を指定します */
+    icon: string
+    /** MinecraftのIDを指定します */
+    minecraftId: string
+    /** DiscordのIDを指定します */
+    discordId: string
+    /** DiscordのURLを指定します */
+    discordUrl: string
+    /** TwitterのIDを指定します */
+    twitterId?: string
+    /** TwitterのURLを指定します */
+    twitterUrl?: string
+  }>(),
+  {
+    twitterId: undefined,
+    twitterUrl: undefined,
+  }
+)
+</script>
+
+<template>
+  <div
+    class="mb-3 flex items-center gap-4 rounded-lg border border-gray-100 p-6"
+  >
+    <div
+      class="not-prose aspect-square w-20 shrink-0 overflow-hidden rounded-xl bg-gray-100"
+    >
+      <img :src="props.icon" alt="icon" class="h-full w-full object-cover" />
+    </div>
+    <div class="grid grow gap-2">
+      <div class="grid gap-2 pl-2">
+        <div class="flex flex-wrap gap-x-2">
+          <NuxtLink
+            :to="`https://users.jaoafa.com/${props.minecraftId}`"
+            class="text-lg font-bold text-current"
+          >
+            {{ props.name }}
+          </NuxtLink>
+          <div
+            class="flex items-center gap-1 font-accent text-xs text-gray-500"
+          >
+            <span>Minecraft: {{ props.minecraftId }}</span>
+          </div>
+        </div>
+
+        <div
+          class="flex flex-wrap items-center gap-x-3 gap-y-1 font-accent text-xs"
+        >
+          <div class="flex items-center gap-2">
+            <AppIcon size="16px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path :d="siDiscord.path" />
+              </svg>
+            </AppIcon>
+            <NuxtLink :to="props.discordId">
+              {{ props.discordId }}
+            </NuxtLink>
+          </div>
+          <div
+            v-if="props.twitterId && props.twitterUrl"
+            class="flex items-center gap-2"
+          >
+            <AppIcon size="16px">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path :d="siTwitter.path" />
+              </svg>
+            </AppIcon>
+            <NuxtLink :to="props.twitterUrl">
+              {{ props.twitterId }}
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div class="text-xs">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
