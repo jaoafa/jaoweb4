ede54f51f26976ad1de3677abb9372b0cb929a9d
Hiratake <hiratake@hiratake.xyz>
2023-06-22 20:39:37 +0900

    chore: 名前とidを横並びにしている箇所のスタイルを調整

--- src/components/content/ContentUser.vue
+++ src/components/content/ContentUser.vue
@@ -34,13 +34,16 @@ const props = withDefaults(
     </div>
     <div class="grid grow gap-2">
       <div class="grid gap-2 pl-2">
-        <div class="flex flex-wrap gap-x-2">
-          <div class="text-lg font-bold text-current">
+        <div class="flex flex-wrap items-center gap-x-2">
+          <div
+            :to="`https://users.jaoafa.com/${props.minecraft}`"
+            class="text-xl font-bold text-current"
+          >
             {{ props.name }}
           </div>
           <NuxtLink
             :to="`https://users.jaoafa.com/${props.minecraft}`"
-            class="flex items-center gap-1 font-accent text-xs text-gray-500"
+            class="translate-y-px font-accent text-xs text-gray-500"
           >
             Minecraft: {{ props.minecraft }}
           </NuxtLink>
